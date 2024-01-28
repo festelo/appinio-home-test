b363c535b5465280f2044d2358a5ab108707ebc6
Ilia B <festeloqq@gmail.com>
2024-01-28 14:51:04 +0100

    integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:appinio_bloc/firebase_options.dart';
 import 'package:appinio_bloc/ui/pages/food_list_page/food_list_cubit.dart';
 import 'package:appinio_bloc/ui/pages/food_list_page/food_list_page.dart';
 import 'package:appinio_bloc/ui/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
